2a3b3d136b53e27af4bb521e4a8617641ce89ef3
Mike Tuntelder <mike.luchies@gmail.com>
2024-11-02 09:13:07 +0100

    removed cron and added timer to main.py

--- Dockerfile
+++ Dockerfile
@@ -3,30 +3,10 @@ FROM python:3.8-slim
 WORKDIR /app
 
 COPY main.py ./
-COPY run_script.sh ./
 COPY requirements.txt .
 
 # install dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Ensure the script is executable
-RUN chmod +x run_script.sh
-
-# Run main.py at container startup
-RUN python3 main.py
-
-# Setup cron job to run every hour
-RUN apt-get update && apt-get install -y cron \
-    && echo "0 * * * * /app/run_script.sh" | crontab -
-
-# Run cron in the foreground
-CMD ["cron", "-f"]
-
-
-
-
-# Copy the main script into the container
-COPY main.py .
-
 # Command to run the script
-CMD ["python", "main.py"]
+CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -2,57 +2,53 @@ import os
 import requests
 import logging
 from datetime import datetime, timedelta
+import time
 
-# Set up logging to console only
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
-# Create console handler
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-
-# Add the console handler to the logger
-logger.addHandler(ch)
+def delete_old_movies(radarr_url, radarr_api_key, days_threshold):
+    """Deletes movies older than the specified threshold from Radarr."""
+    # Make API call to Radarr
+    response = requests.get(f"{radarr_url}/api/v3/movie", headers={"X-Api-Key": radarr_api_key})
+    response.raise_for_status()
+    
+    current_date = datetime.utcnow()
+    threshold_date = current_date - timedelta(days=days_threshold)
+    
+    for movie in response.json():
+        added_date = datetime.strptime(movie.get("added"), '%Y-%m-%dT%H:%M:%SZ')
+        if added_date < threshold_date:
+            delete_response = requests.delete(f"{radarr_url}/api/v3/movie/{movie['id']}", headers={"X-Api-Key": radarr_api_key})
+            if delete_response.status_code == 200:
+                logger.info(f"Deleted movie: {movie['title']} (ID: {movie['id']})")
+            else:
+                logger.error(f"Failed to delete movie: {movie['title']} (ID: {movie['id']}), Status Code: {delete_response.status_code}")
 
 def main():
     logger.info("Starting Radarr Clean Sweep script.")
-
+    
     # Retrieve environment variables
     radarr_url = os.getenv("RADARR_URL")
     radarr_api_key = os.getenv("RADARR_API_KEY")
+    sleep_duration = int(os.getenv('SLEEP_DURATION', 3600))
     days_threshold = int(os.getenv("DAYS", 60))
 
     if not radarr_url or not radarr_api_key:
         logger.error("RADARR_URL and RADARR_API_KEY must be set.")
         return
 
-    # Make API call to Radarr
     try:
-        response = requests.get(f"{radarr_url}/api/v3/movie", headers={"X-Api-Key": radarr_api_key})
-        response.raise_for_status()  # Raise an error for bad responses
-        movies = response.json()
-
-        # Current date for comparison
-        current_date = datetime.utcnow()
-        threshold_date = current_date - timedelta(days=days_threshold)
-
-        for movie in movies:
-            added_date = datetime.strptime(movie.get("added"), '%Y-%m-%dT%H:%M:%SZ')  # Correct date format
-            if added_date < threshold_date:
-                # Logic to delete the movie
-                movie_id = movie['id']
-                delete_response = requests.delete(f"{radarr_url}/api/v3/movie/{movie_id}", headers={"X-Api-Key": radarr_api_key})
-                if delete_response.status_code == 200:
-                    logger.info(f"Deleted movie: {movie['title']} (ID: {movie_id})")
-                else:
-                    logger.error(f"Failed to delete movie: {movie['title']} (ID: {movie_id}), Status Code: {delete_response.status_code}")
-
+        delete_old_movies(radarr_url, radarr_api_key, days_threshold)
         logger.info("Radarr Clean Sweep script completed.")
+    except requests.HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        logger.error(f"An unexpected error occurred: {err}")
 
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
+    logger.info(f"Sleeping for {sleep_duration} seconds...")
+    time.sleep(sleep_duration)
 
 if __name__ == "__main__":
     main()
